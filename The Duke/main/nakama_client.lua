c9baa2aef795572fd51735d09ecd2ec794aadca5
sage <user.name>
2025-06-30 10:02:04 -0700

    create initial client file

--- The Duke/main/nakama_client.lua
+++ The Duke/main/nakama_client.lua
@@ -1,5 +1,6 @@
 local defold = require "nakama.engine.defold"
 local nakama = require "nakama.nakama"
+
 local config = {
     host = "127.0.0.1",
     port = 7350,
@@ -9,4 +10,22 @@ local config = {
     engine = defold,
     timeout = 10, -- connection timeout in seconds
 }
-local client = nakama.create_client(config)
+
+local M = {}
+
+M.client = nakama.create_client(config)
+M.socket = nakama.create_socket(M.client)
+
+nakama.sync(function()
+    -- connect
+    local ok, err = nakama.socket_connect(M.socket)
+
+    if ok then
+      -- do socket stuff
+    end
+
+    if err then
+      print(err.message)
+    end
+end)
+

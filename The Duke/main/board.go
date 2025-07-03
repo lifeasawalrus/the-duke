682a4f836ebab679de5673c69ff98891fb22ace7
sage <user.name>
2025-07-03 15:58:29 -0700

    add board game object

---
+++ The Duke/main/board.go
@@ -0,0 +1,19 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/assets/board/dukeBoard.glb\"\n"
+  "name: \"{{NAME}}\"\n"
+  "materials {\n"
+  "  name: \"dukeBoard\"\n"
+  "  material: \"/builtins/materials/model.material\"\n"
+  "  textures {\n"
+  "    sampler: \"tex0\"\n"
+  "    texture: \"/assets/board/dukeBoardColored.png\"\n"
+  "  }\n"
+  "}\n"
+  ""
+  rotation {
+    x: 0.70710677
+    w: 0.70710677
+  }
+}
